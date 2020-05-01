bc2b6ff4de05a7972ab1f571c51d4bfe225b8e92
dwelchnait <dwelch@nait.ca>
2020-05-01 12:32:05 -0600

    try out console in Core

---
+++ BranchingSolution/Branching/Program.cs
@@ -0,0 +1,30 @@
+﻿//import other libraries required for program
+//these are namespaces
+using System;
+
+//used to group and organize code
+namespace Branching
+{
+    class Program
+    {
+        //entry point into your program
+
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            //interesting doing a relative test outside of if
+            //bool result = a + b > 10;
+            //if (result)
+            if (a + b > 10)
+            {
+                Console.WriteLine("Athis answer is greater than 10.");
+            }
+            else
+            {
+                Console.WriteLine("Athis answer is not greater than 10.");
+            }
+        }
+    }
+}
