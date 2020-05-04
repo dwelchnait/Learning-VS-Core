3ac2e4334f2fcc23873d0937832a4a0e1619a6dd
dwelchnait <dwelch@nait.ca>
2020-05-03 20:31:32 -0600

    Update json data file and Model file to match videos using source from fithub account

--- ContosoCrafts/ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts/ContosoCrafts.WebSite/Models/Product.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
     public class Product
     {
-        //add a JsonPropertyName notation to map C# property names which are different then the Json data file attribute name
-        [JsonPropertyName("Id")]
-        public string ProductId { get; set; }
+        public string Id { get; set; }
         public string Maker { get; set; }
-       
+
+        //use this annnotation to map a property name to the json attribute if they are different
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
 
-        //public override string ToString()
-        //{
-        //    return JsonSerializer.Serialize<Product>(this);
-        //}
-
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
-        
+
+ 
     }
 }
