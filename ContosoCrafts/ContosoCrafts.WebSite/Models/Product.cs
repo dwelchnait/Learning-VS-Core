97210a18592bece34e075d438d8b61abdb590b22
dwelchnait <dwelch@nait.ca>
2020-05-03 19:49:41 -0600

    create aspnet core webi application

---
+++ ContosoCrafts/ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("Id")]
+        public string ProductId { get; set; }
+        public string Maker { get; set; }
+       
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return JsonSerializer.Serialize<Product>(this);
+        //}
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+    }
+}
