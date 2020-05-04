f2bc1ae8a98f1aea19515dbc815452941b327da8
dwelchnait <dwelch@nait.ca>
2020-05-03 20:47:41 -0600

    Display data from json file. see INdexModel and OnGet methods. Raxor foreeach on web page

--- ContosoCrafts/ContosoCrafts.WebSite/Pages/Index.cshtml.cs
+++ ContosoCrafts/ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,25 @@ namespace ContosoCrafts.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //declaring to .Net that a service is required, JsonFileProductService
+        //ILogger is a .Net service that allows for logging
+        public IndexModel(ILogger<IndexModel> logger, 
+            JsonFileProductService productService)
         {
             _logger = logger;
+            //get an instance of your custom service
+            ProductService = productService;
         }
 
+        //event handler when the page is retrieved
         public void OnGet()
         {
-
+            //
+            //get the products
+            Products = ProductService.GetProducts();
         }
     }
 }
