62ac4df33d961a753b974dd6f822b61339e874ed
Don Welch <dwelch@nait.ca>
2020-11-14 15:27:19 -0700

    random program to assign 1517 scenarios

--- SampleCoreFirst/SampleCore/Program.cs
+++ SampleCoreFirst/SampleCore/Program.cs
@@ -3,54 +3,77 @@
 //done to try to make learning simple programming concepts without
 //          the a program shell showing
 
+using System;
 using System.IO;
 using static System.Console;
+
+string[] students = new string[] { "Gartner", "Olthoff", "Kozmak", "Zielke", "Gauthier", "Murray", "Chen", "Emmanuel" };
+
+string[] otherstudents = new string[] { "Best", "Brower", "Cairns", "Del Colle", "Neufeld", "Pang", "Popik", "Sayong", "Walton",  "Wu"};
+
+string[] nonstudents = new string[] { "lopez", "Riad", "Sylvester", "Tran", "Wilber" };
+
+Random rdn = new Random();
     
-WriteLine("Hello World from .Net 5");
-
-//you still have access to thing like the args on the Main()
-WriteLine(args.Length);
-
-//using the new concept of record datatype
-//think LinqPad Program ide: 1 the body 2 then definitions
-
-//body of main method - Executable Statements
-var friend = new Person("Don", "Welch", 65);
-WriteLine(friend);
-//int years = PromptUser("How long have you known Don:\t");
-//WriteLine($"{years} is a long time");
-
-string data = friend.ToString();
-WriteLine(data);
-
-//Deconstructor of record
-var (first, last, age) = friend;
-WriteLine($"first={first} last={last} age={age}");
-
-//file i/o
-//use relative addressing to change the location of output
-//default is in bin/xxxx/net5.0  where xxxx is either Debug or Release
-//      wherever the .exe is located
-
-//output
-File.WriteAllText("../../../Demo", friend.ToString());
-//input
-int total = 0;
-foreach(string fileline in File.ReadAllLines("../../../NumberFile.txt"))
+int index = 0;
+int assigned = 0;
+string accept = "";
+while (assigned < otherstudents.Length)
 {
-    int num = int.Parse(fileline);
-    total += num;
+    index = rdn.Next(1, otherstudents.Length);
+    WriteLine($"student {otherstudents[index - 1]}, accept (y,n)\t");
+    accept = ReadLine();
+    if (accept.ToUpper().Equals("Y"))
+    {
+        assigned++;
+    }
 }
-WriteLine($"total is {total}");
+    
+//WriteLine("Hello World from .Net 5");
 
-//programmer defined methods and types - declartions
+////you still have access to thing like the args on the Main()
+//WriteLine(args.Length);
 
-//methods 1st
-int PromptUser(string message)
-{
-    Write(message);
-    return int.Parse(ReadLine());
-}
+////using the new concept of record datatype
+////think LinqPad Program ide: 1 the body 2 then definitions
 
-//types 2nd
-record Person(string FirstName, string LastName, int Age);
+////body of main method - Executable Statements
+//var friend = new Person("Don", "Welch", 65);
+//WriteLine(friend);
+////int years = PromptUser("How long have you known Don:\t");
+////WriteLine($"{years} is a long time");
+
+//string data = friend.ToString();
+//WriteLine(data);
+
+////Deconstructor of record
+//var (first, last, age) = friend;
+//WriteLine($"first={first} last={last} age={age}");
+
+////file i/o
+////use relative addressing to change the location of output
+////default is in bin/xxxx/net5.0  where xxxx is either Debug or Release
+////      wherever the .exe is located
+
+////output
+//File.WriteAllText("../../../Demo", friend.ToString());
+////input
+//int total = 0;
+//foreach(string fileline in File.ReadAllLines("../../../NumberFile.txt"))
+//{
+//    int num = int.Parse(fileline);
+//    total += num;
+//}
+//WriteLine($"total is {total}");
+
+////programmer defined methods and types - declartions
+
+////methods 1st
+//int PromptUser(string message)
+//{
+//    Write(message);
+//    return int.Parse(ReadLine());
+//}
+
+////types 2nd
+//record Person(string FirstName, string LastName, int Age);
