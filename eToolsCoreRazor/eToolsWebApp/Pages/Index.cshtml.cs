891b394aa8eba7333820363e0c6ab4379570e05e
Don Welch <dwelch@nait.ca>
2021-03-28 16:38:01 -0600

    learning about core 5 web application with razor pages

---
+++ eToolsCoreRazor/eToolsWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eToolsWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; } 
+        [BindProperty]
+        public string CourseNumber { get; set; }
+        [BindProperty]
+        public DateTime StartDate { get; set; }
+        [BindProperty]
+        public int Rating { get; set; }
+
+        public void OnGet()
+        {
+            CourseNumber = "DMIT-2018";
+
+        }
+        public void OnPost()
+        {
+            Message = $"You stated <b>{CourseNumber}</b> on <b>{StartDate.ToShortDateString()}</b> and rated the difficulty as <b>{Rating}</b>/5.";
+        }
+    }
+}
